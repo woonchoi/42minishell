748d769f44c54f065b99e7f3245aa91dd5b4642c
woonchoi <42.4.woonchoi@gmail.com>
2022-06-05 12:46:12 +0900

    parser[work in progress]

--- srcs/parser/parser.c
+++ srcs/parser/parser.c
@@ -6,7 +6,7 @@
 /*   By: woonchoi <woonchoi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/25 16:12:52 by woonchoi          #+#    #+#             */
-/*   Updated: 2022/06/05 12:35:30 by woonchoi         ###   ########.fr       */
+/*   Updated: 2022/06/05 12:45:35 by woonchoi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,11 +14,28 @@
 
 void	init_tree_with_pipecount(t_mshell_info *info)
 {
-	//need to make function
+	int	pipecount;
+	int	pipesize;
+
+	if (info->error)
+		return ;
+	pipecount = info->cmd_count + 2;
+	pipesize = sizeof(t_tree_list);
+	info->head = (t_tree_list *)calloc(pipecount, pipesize);
+	if (!pipecount)
+	{
+		info->error = TRUE;
+		safety_free(info->head);
+	}
 }
+
+
 
 void	build_tree(t_mshell_info *info)
 {
+	init_tree_with_pipecount(info);
+	if (info->error)
+		return ;
 	//need to make function
 }
 
