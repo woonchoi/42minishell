9886db57c34f3e82d56da3c669726e400bda9dfe
woonchoi <42.4.woonchoi@gmail.com>
2022-06-05 12:37:32 +0900

    add WIP function and delete remain function in parser.c

--- srcs/builtin/builtin_ls.c
+++ srcs/builtin/builtin_ls.c
@@ -1,29 +1,18 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   parser.c                                           :+:      :+:    :+:   */
+/*   builtin_ls.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: woonchoi <woonchoi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/05/25 16:12:52 by woonchoi          #+#    #+#             */
-/*   Updated: 2022/06/05 12:35:30 by woonchoi         ###   ########.fr       */
+/*   Created: 2022/06/05 12:35:54 by woonchoi          #+#    #+#             */
+/*   Updated: 2022/06/05 12:36:44 by woonchoi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/minishell.h"
 
-void	init_tree_with_pipecount(t_mshell_info *info)
+int		builtin_ls(t_mshell_info *info)
 {
-	//need to make function
-}
-
-void	build_tree(t_mshell_info *info)
-{
-	//need to make function
-}
-
-void	parser(t_mshell_info *info)
-{
-	if (info->error)
-		return ;
+	
 }
