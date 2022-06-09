75dafbafdb8ec558e405a733847f4fd23a31dbed
jasong <jasong@student.42seoul.kr>
2022-06-09 21:51:39 +0900

    cd without path fixed

--- srcs/builtin/builtin_cd.c
+++ srcs/builtin/builtin_cd.c
@@ -6,7 +6,7 @@
 /*   By: jasong <jasong@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/07 21:26:19 by jasong            #+#    #+#             */
-/*   Updated: 2022/06/08 19:40:27 by jasong           ###   ########.fr       */
+/*   Updated: 2022/06/09 21:51:25 by jasong           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,8 +16,8 @@ int	builtin_cd(char *path, t_mshell_info *info)
 {
 	int	ret;
 
-	// if (!path || !path[0])
-	// 	path = home_path(info); // homepath 작성해야함...
+	if (!path)
+		path = get_value_with_key("HOME", info->env_head);
 	ret = chdir(path);
 	if (ret == -1)
 	{
