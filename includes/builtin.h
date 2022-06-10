e06f1a1d41d09542717fd42f53accad8e501299e
jasong <jasong@student.42seoul.kr>
2022-06-10 16:00:12 +0900

    builtin header modified

--- includes/builtin.h
+++ includes/builtin.h
@@ -6,7 +6,7 @@
 /*   By: jasong <jasong@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/09 17:08:33 by jasong            #+#    #+#             */
-/*   Updated: 2022/06/09 21:23:33 by jasong           ###   ########.fr       */
+/*   Updated: 2022/06/10 15:59:07 by jasong           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,12 @@
 
 int		builtin_cd(char *path, t_mshell_info *info);
 int		builtin_echo(char *argv[]);
+int		builtin_env(char *argv[], t_env_list *env_head);
+int		builtin_exit(char *argv[], int pipe_flag);
+int		builtin_export(char *argv[], t_mshell_info *info);
+int		builtin_pwd(void);
+int		builtin_unset(t_env_list *env_head, char *argv[]);
+
 void	ft_error(char *cmd, char *cmd_arg, char *errmsg);
 void	ft_s_quote_error(char *cmd, char *cmd_arg, char *errmsg);
 int		check_avaliable_key(char *key);
